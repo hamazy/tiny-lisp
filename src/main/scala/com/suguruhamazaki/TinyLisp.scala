986e3c92757ce18a64aaa1a48a35f59bdbc30c83
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2015-02-11 17:54:48 +0900

    Implement programs entry point.

--- src/main/scala/com/suguruhamazaki/TinyLisp.scala
+++ src/main/scala/com/suguruhamazaki/TinyLisp.scala
@@ -1,7 +1,26 @@
 package com.suguruhamazaki
 
-object TinyLisp {
+import scala.io.Source
+
+object TinyLisp extends Parsers with FormEvaluator {
+  def globalEnvironment: Map[Symbol, Form] =
+    Map(
+      Symbol("+") → Plus,
+      Symbol("and") → And,
+      Symbol("not") → Not,
+      Symbol("lambda") → Lambda
+    )
   def main(args: Array[String]): Unit = {
-    println("Hello, World")
+    val input = Source.stdin.mkString
+    val parsed = parseAll(form, input).map {
+      case a: Atom ⇒ Evaluator.eval(a, globalEnvironment)
+      case n: Forms ⇒ Evaluator.eval(n, globalEnvironment)
+    }
+    val evaluated = parsed match {
+      case Success(result, _) ⇒ result
+      case error: NoSuccess ⇒ scala.util.Failure(new RuntimeException(error.msg))
+    }
+    val result = evaluated.map(_.toString).recover { case ex ⇒ ex.getMessage }.get
+    println(result)
   }
 }
