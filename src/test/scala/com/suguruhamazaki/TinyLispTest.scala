68b8dd4d9ac0e5cc3ba7fc7b839f47d268015d19
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2015-02-12 03:08:10 +0900

    Add more tests.

--- src/test/scala/com/suguruhamazaki/TinyLispTest.scala
+++ src/test/scala/com/suguruhamazaki/TinyLispTest.scala
@@ -23,4 +23,58 @@ class TinyLispTest extends FlatSpec with GivenWhenThen with Matchers {
 
     actual should be(Success("6765"))
   }
+
+  it should "compute addition" in {
+    val input = "(+ 1 2)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("3"))
+  }
+
+  it should "compute division" in {
+    val input = "(/ 4 2)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("2"))
+  }
+
+  it should "compute division with truncation" in {
+    val input = "(/ 5 3)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("1"))
+  }
+
+  it should "compute logical and" in {
+    val input = "(and t nil t)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("nil"))
+  }
+
+  it should "compute logical or" in {
+    val input = "(or -3 nil t)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("-3"))
+  }
+
+  it should "compute not" in {
+    val input = "(not 123)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("nil"))
+  }
+
+  it should "compute lower than" in {
+    val input = "(< 1 2)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("t"))
+  }
+
+  it should "compute equals" in {
+    val input = "(= 0 0)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("t"))
+  }
+
+  it should "compute conditional" in {
+    val input = "(if (or nil t) 123 unbound)"
+    val actual = TinyLisp.parseAndEval(input)
+    actual should be(Success("123"))
+  }
 }
