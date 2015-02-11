49f70e4199d149f30a6887e83ba81f69da2e006a
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2015-02-11 23:59:04 +0900

    Allow negative numbers.

--- src/main/scala/com/suguruhamazaki/Parser.scala
+++ src/main/scala/com/suguruhamazaki/Parser.scala
@@ -8,8 +8,9 @@ trait Parsers extends JavaTokenParsers {
   def form: Parser[Form] = atom | "(" ~> forms <~ ")" | "(" ~ ")" ^^ (_ ⇒ Nil)
   def atom: Parser[Atom] = number | boolean | symbol
   def forms: Parser[Form] = rep1(form) ^^ (fs ⇒ Forms(fs))
-  def number: Parser[Number] = decimalNumber ^^ { s ⇒
-    Try(Integer(s.toInt)).getOrElse(Double(s.toDouble))
+  def number: Parser[Number] = "-".? ~ decimalNumber ^^ { s ⇒
+    val n = s._1.map(_ + s._2).getOrElse(s._2)
+    Try(Integer(n.toInt)).getOrElse(Double(n.toDouble))
   }
   def boolean: Parser[Atom] = "nil" ^^ (_ ⇒ Nil) | "t" ^^ (_ ⇒ True)
   // any visible characters except for `(` and `)`.
